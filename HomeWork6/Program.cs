40dddf64b727b2ebaed8c3db2d1458541cb3cb06
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-08-03 20:15:51 +0300

    Add project files.

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // *** 100'den geriye doğru 11 sayısına kadar sayıları 11 azaltarak ekranda gösteriniz ***
+
+            for (int i = 100; i >= 11; i-=11)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("****");
+
+            // *** 1'den 100'e kadar olan sayıları ekranda for ile yazdırınız ***/
+
+            for (int j = 1; j <= 100; j++)
+            {
+                Console.WriteLine(j);
+            }
+
+            Console.Read();
+        }
+    }
+}
